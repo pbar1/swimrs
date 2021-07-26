5c42f4a904effa483c069b9d2b06d6010d320f08
Pierce Bartine <piercebartine@gmail.com>
2021-07-26 12:54:06 -0700

    Add log dependency

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,8 @@ const VERSION: Option<&'static str> = option_env!("CARGO_PKG_VERSION");
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
+    env_logger::init();
+
     let matches = App::new("swimrs")
         .version(VERSION.unwrap_or("unknown"))
         .author("Pierce Bartine")
@@ -15,6 +17,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
         .setting(AppSettings::SubcommandRequiredElseHelp)
         .subcommand(App::new("toptimes").about("Top Times / Event Rank Search"))
         .subcommand(App::new("indtimes").about("Individual Times Search"))
+        .subcommand(App::new("mirror").about("Download all times from USA Swimming"))
         .get_matches();
 
     match matches.subcommand_name() {
